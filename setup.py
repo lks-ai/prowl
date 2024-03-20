84aceaf35e365fa5592e7a80f710829ff6906b6f
holonic <nathaniel.gibson@gmail.com>
2024-03-20 09:48:28 +0000

    refactor

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='prompt-owl',
     version='0.1.2',
-    packages=find_packages(),
+    packages=find_packages(where='.'),
     install_requires=[
         'requests>=2.31.0',
         'aiohttp>=3.9.1',
@@ -18,7 +21,7 @@ setup(
     author='LoreKeeper Ltd',
     author_email='nathaniel@lorekeeper.co.uk',
     description='A Declarative Prompting Language for LLMs',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/lks-ai/prowl',
     license='MIT',
