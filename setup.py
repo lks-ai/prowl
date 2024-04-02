89f0fbe56417e86fe0037562e8aa928eba959b6e
holonic <nathaniel.gibson@gmail.com>
2024-04-02 20:17:43 +0000

    setup thingy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='prompt-owl',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(where='.'),
     install_requires=[
         'requests>=2.31.0',
